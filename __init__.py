cf646981988b2f6394c998baa5bd1fd5612708ed
raTar <ch.thitirat_st@tni.ac.th>
2016-12-16 03:41:16 +0000

    3rd Commit

--- __init__.py
+++ __init__.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 
 from random import randint
-
+mes = "\0"
 
 for x in range(6):
-    print str(randint(0,9))
+    mes = mes + str(randint(0,9))
+    
+print "Lotto number is >> %s <<" % mes
+mes1 =[]
+
+for x in range(4):
+    mes2 = ""
+    for y in range(3):    
+        mes2 = mes2 + str(randint(0,9))
+        
+        
+    mes1.append(mes2)
+    mes2 = ""
+print "Lotto number is >> %s <<" % mes1
